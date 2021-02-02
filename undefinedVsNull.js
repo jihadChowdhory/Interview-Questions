852f673d195c3046be8deab60fb2ed8b59548ebd
jihadChowdhory <mohammadjihad0333@gmail.com>
2021-02-02 21:24:17 +0600

    undefined vs null finished

---
+++ undefinedVsNull.js
@@ -0,0 +1,16 @@
+let age;
+console.log(age);
+
+function add(num1, num2) {
+  total = num1 + num2;
+}
+console.log(add(1, 2));
+
+let name = undefined;
+console.log(name);
+
+const list = [1, 2, 3, 4, 5];
+console.log(list[10]);
+
+const object = { name: 'Jihad', roll: 9 };
+console.log(object.class);

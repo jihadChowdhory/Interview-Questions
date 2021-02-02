061e53e4ea28b5178c7db4366a8441dd44c1874b
jihadChowdhory <mohammadjihad0333@gmail.com>
2021-02-02 22:06:32 +0600

    finished double equal and triple equal concepts

---
+++ doubleVsTriple.js
@@ -0,0 +1,53 @@
+const a = 2;
+const b = '2';
+if (a == b) {
+  console.log('condition is true');
+}
+else {
+  console.log('condition is false');
+}
+
+const c = 2;
+const d = '2';
+if (c === d) {
+  console.log('condition is true');
+}
+else {
+  console.log('condition is false');
+}
+
+const e = 1;
+const f = true;
+if (e == f) {
+  console.log('condition is true');
+}
+else {
+  console.log('condition is false');
+}
+
+const g = 1;
+const h = true;
+if (g === h) {
+  console.log('condition is true');
+}
+else {
+  console.log('condition is false');
+}
+
+const i = 0;
+const j = false;
+if (i == j) {
+  console.log('condition is true');
+}
+else {
+  console.log('condition is false');
+}
+
+const k = 0;
+const l = false;
+if (k === l) {
+  console.log('condition is true');
+}
+else {
+  console.log('condition is false');
+}
